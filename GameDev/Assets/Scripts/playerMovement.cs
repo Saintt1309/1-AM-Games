5f3efe8d06ff9bfabc34c61f64bbb61bd0d0d370
ets47 <ertsberg.tjahaya@gmail.com>
2025-05-29 01:00:42 +0800

    Update playerMovement.cs

--- GameDev/Assets/Scripts/playerMovement.cs
+++ GameDev/Assets/Scripts/playerMovement.cs
@@ -2,63 +2,155 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpForce = 6.2f;
-    private bool isFacingRight = true;
-
-    private bool airJump;
-
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float acceleration = 7f;
+    [SerializeField] private float deceleration = 7f;
+    [SerializeField] private float velocityPower = 0.9f;
+    [SerializeField] private float frictionAmount = 0.2f;
+    
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 6.2f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+    [SerializeField] private float fallGravityMultiplier = 1.2f;
+    [SerializeField] private int maxAirJumps = 1;
+    [SerializeField] private float coyoteTime = 0.2f;
+    [SerializeField] private float jumpBufferTime = 0.2f;
+    
+    [Header("References")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    
+    // Private variables
+    private float horizontal;
+    private bool isFacingRight = true;
+    private int airJumpCount;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+    private bool isJumping;
+    private float defaultGravityScale;
+    
+    private void Awake()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && isGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-
-        if (Input.GetButtonDown("Jump") && rb.linearVelocity.y > 0)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-
-        if (isGrounded())
-        {
-            airJump = true;
-        }
-
-        Flip();
-
-        if (Input.GetButtonDown("Jump") && airJump)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            airJump = false;
-        }
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+            
+        defaultGravityScale = rb.gravityScale;
     }
+    
+    private void Update()
+    {
+        // Input handling
+        horizontal = Input.GetAxisRaw("Horizontal");
+        
+        // Jump input buffer
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+        
+        // Jump cut (variable jump height)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0 && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpCutMultiplier);
+            isJumping = false;
+        }
+        
+        // Coyote time handling
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+            airJumpCount = 0;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        
+        // Jump execution
+        if (jumpBufferCounter > 0)
+        {
+            // Ground jump or coyote time jump
+            if (coyoteTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                isJumping = true;
+                jumpBufferCounter = 0;
+            }
+            // Air jump
+            else if (airJumpCount < maxAirJumps)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                isJumping = true;
+                airJumpCount++;
+                jumpBufferCounter = 0;
+            }
+        }
+        
+        // Gravity scale adjustment for better jump feel
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = defaultGravityScale * fallGravityMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = defaultGravityScale;
+        }
+        
+        // Handle character facing direction
+        UpdateFacingDirection();
+    }
+    
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
-
+        // Apply movement with better physics
+        ApplyMovement();
     }
-
-    private bool isGrounded()
+    
+    private void ApplyMovement()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        // Calculate target speed
+        float targetSpeed = horizontal * moveSpeed;
         
+        // Calculate speed difference
+        float speedDifference = targetSpeed - rb.velocity.x;
+        
+        // Calculate acceleration rate based on direction
+        float accelerationRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        
+        // Calculate movement force
+        float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelerationRate, velocityPower) * Mathf.Sign(speedDifference);
+        
+        // Apply movement force
+        rb.AddForce(movement * Vector2.right);
+        
+        // Apply friction when not pressing movement keys
+        if (Mathf.Abs(horizontal) < 0.01f && IsGrounded())
+        {
+            float frictionForce = Mathf.Min(Mathf.Abs(rb.velocity.x), frictionAmount);
+            frictionForce *= Mathf.Sign(rb.velocity.x);
+            rb.AddForce(Vector2.right * -frictionForce, ForceMode2D.Impulse);
+        }
     }
-    private void Flip()
+    
+    private bool IsGrounded()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0)
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+    
+    private void UpdateFacingDirection()
+    {
+        if ((isFacingRight && horizontal < 0) || (!isFacingRight && horizontal > 0))
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
-            localScale.x = -1f;
+            localScale.x *= -1;
             transform.localScale = localScale;
         }
     }
