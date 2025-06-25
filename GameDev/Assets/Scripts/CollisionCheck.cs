195078c4fef2fdc96562d35b039bec1e983a1feb
Saintt1309 <30825508+Saintt1309@users.noreply.github.com>
2025-06-26 00:09:28 +0800

    Added Spike Collision
    Added spikes and collision states for when the player collides with spikes and possible other hazards.

--- GameDev/Assets/Scripts/CollisionCheck.cs
+++ GameDev/Assets/Scripts/CollisionCheck.cs
@@ -20,6 +20,15 @@ public class CollisionCheck : MonoBehaviour
     public float collisionRadius = 0.25f;
     public Vector2 bottomOffset, rightOffset, leftOffset;
     private Color debugCollisionColor = Color.red;
+
+    [Space]
+
+    [Header("Spike Collision")]
+
+    public PhysicsMaterial2D spikeMaterial;
+    public LayerMask spikeLayer;
+    public bool touchingSpike;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -37,18 +46,35 @@ public class CollisionCheck : MonoBehaviour
 
         wallSide = onRightWall ? -1 : 1;
 
+        
     }
 
 
-    
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
         var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
 
-        Gizmos.DrawWireSphere((Vector2)transform.position  + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, collisionRadius);
         Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
         Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & spikeLayer) != 0)
+        {
+            // The (1 << collision.gameObject.layer) creates a bitmask for the collided object's layer.
+            // The & operator checks if any bits overlap with 'spikeLayer'.
+            // If they do, it means the collided object is on a layer included in 'spikeLayer'.
+            Debug.Log("Hit by spikes (Layer Check)!");
+            touchingSpike = true;
+        }
+        else
+        {
+            touchingSpike = false;
+        }
     }
 }
