f55bc8253cbf0478b4758186aefb0a80f910ebe7
Saintt1309 <30825508+Saintt1309@users.noreply.github.com>
2025-07-03 15:12:41 +0800

    Commented unused file

--- GameDev/Assets/Scripts/playerMovement.cs
+++ GameDev/Assets/Scripts/playerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+/*public class PlayerMovement : MonoBehaviour
 {
     
     
@@ -91,3 +91,4 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
+*/
